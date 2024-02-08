1ad48ecd450abc14c6331de3222840dc3b6afcfa
AliaksandrHrychonak <@icloud.com>
2024-02-08 19:36:59 +0300

    fix(category): duplicate entity removed

--- src/modules/category/dtos/category.import.dto.ts
+++
@@ -1,7 +0,0 @@
-import { OmitType } from '@nestjs/swagger';
-import { CategoryCreateDto } from './category.create.dto';
-
-export class CategoryImportDto extends OmitType(
-    CategoryCreateDto,
-    [] as const
-) {}
--- src/modules/category/dtos/category.request.dto.ts
+++
@@ -1,9 +0,0 @@
-import { Type } from 'class-transformer';
-import { IsNotEmpty, IsUUID } from 'class-validator';
-
-export class CategoryRequestDto {
-    @IsNotEmpty()
-    @IsUUID('4')
-    @Type(() => String)
-    category: string;
-}
